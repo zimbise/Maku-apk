dc35e4269ac0cbc53f20f2b0644ae6651e010ad6
ZimBuss <zim@codearchitect.dev>
2025-11-05 13:54:56 -0500

    Manual WebView APK wrapper with magnet launcher

---
+++ android/app/src/main/java/com/zimbuss/maku/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zimbuss.maku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
